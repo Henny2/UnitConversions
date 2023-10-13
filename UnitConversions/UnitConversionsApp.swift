2546ae8a1bd56444a9c6bccab868ea419f07d02e
Henny2 <henrieke.baunack@web.de>
2023-10-12 19:14:24 -0700

    Initial Commit

---
+++ UnitConversions/UnitConversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionsApp.swift
+//  UnitConversions
+//
+//  Created by Henrieke Baunack on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
